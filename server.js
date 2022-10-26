f14aa865f2321b9f389d39b7a0c44a4fa3f9e1b5
unknown <hamza>
2022-10-26 22:13:45 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.listen(8080);
+
